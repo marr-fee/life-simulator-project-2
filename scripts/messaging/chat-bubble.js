6b9413e9ba07fbdad4a064623031e3919922cb9f
marr-fee <kennethohunwu@gmail.com>
2025-04-07 22:45:48 +0200

    fixed the chat bubble

--- scripts/messaging/chat-bubble.js
+++ scripts/messaging/chat-bubble.js
@@ -7,16 +7,19 @@ function scrollToBottom() {
 }
 
 // Example of adding a new message dynamically
-function addMessage(message, senderBlock, senderBbl ) {
+function addMessage(message, imageDp, senderBlock, senderBbl, imgVisibility ) {
     const messageBlockElement = document.createElement('div');
     messageBlockElement.classList.add(senderBlock);
-
+    const receiverDp = document.createElement('img');
+    receiverDp.src = imageDp;
+    receiverDp.classList.add('chat-bbl-dp-img');
+    receiverDp.style.visibility = imgVisibility;
     const messageBubble = document.createElement('div');
     messageBubble.classList.add(senderBbl);
     const messageText = document.createElement('p');
     messageText.textContent = message;
     messageBubble.appendChild(messageText);
-    messageBlockElement.appendChild(messageBubble);
+    messageBlockElement.append(receiverDp, messageBubble);
 
     chatContainer.appendChild(messageBlockElement);
     scrollToBottom(); // Ensure the chat container scrolls to the bottom
@@ -26,8 +29,11 @@ function addMessage(message, senderBlock, senderBbl ) {
 setInterval(() => {
   const senderBlock = Math.random() > 0.5 ? 'my-chat-bbl-cntr' : 'npc-chat-bbl-contnr';
   const senderBubble = senderBlock === 'my-chat-bbl-cntr' ? 'my-chat-bubble' : 'npc-chat-buble';
+  const imageDp = senderBubble === 'npc-chat-buble' ? 'scripts/messaging/images/profile(1).png' : '';
+  const imgVisibility = senderBubble === 'npc-chat-buble' ? 'visible' : 'hidden';
+  
 
-//addMessage('New message', senderBlock, senderBubble);
+  addMessage('New message', imageDp,  senderBlock, senderBubble, imgVisibility);
 
   
   // Initial scroll to the bottom
